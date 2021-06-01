09521de62dc1980781710417d23018fbb98019a1
Liz Thompson <thompsonel2011@gmail.com>
2021-06-01 09:54:43 -0400

    remove sentence

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const questions = [
     {
       type: 'input',
       name: 'contribute',
-      message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if youd prefer.',
+      message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.',
     },
     {
       type: 'input',
